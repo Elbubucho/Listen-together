46d9203c858006a980420466ac60eaed03a35892
Emma Rougier <rougier.emma@hotmail.fr>
2025-06-10 23:32:31 +0200

    added routes, new & create methods, partial comment and show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,26 @@
 class CommentsController < ApplicationController
-  def create
+  before_action :set_post, only: %i[new create]
 
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    if @comment.save
+      redirect_to  post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  resources :posts do
+    resources :comments, only: [:new :create]
+  end
 end
