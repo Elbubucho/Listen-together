e3ee5c5b07b7cc374d5c8f954885b7c60762ab24
Valentin Sala <valmax.sala@gmail.com>
2025-06-11 13:25:20 +0200

    azda

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,20 +6,4 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
-  def new
-
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
