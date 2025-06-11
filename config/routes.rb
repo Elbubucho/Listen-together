723ef465ae5ee94c55b97c92f301d3977760afa0
Valentin Sala <valmax.sala@gmail.com>
2025-06-11 18:00:20 +0200

    add username field to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "posts#index"
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resource :reaction, only: [:create, :destroy]
   end
-  
+
    get 'tracks/autocomplete', to: 'posts#autocomplete'
-  
+
 end
