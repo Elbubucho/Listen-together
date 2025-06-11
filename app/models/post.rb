7ba9588f8ff6fa5771d9da4889ae43634415596d
Valentin Sala <valmax.sala@gmail.com>
2025-06-11 13:20:33 +0200

    show post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  MOODS = ["😊", "😢", "🤩", "😎", "😡"]
+
   has_many :comments, dependent: :destroy
   has_many :reactions, dependent: :destroy
 
   validates :content, presence: true
   validates :user_id, presence: true
   validates :track_id, presence: true
+  validates :mood, presence: true, inclusion: { in: MOODS }
 end
