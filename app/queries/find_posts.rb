f7b28dc4e32ee879e3ec7a107929a8e3b98a9b67
Valentin Sala <valmax.sala@gmail.com>
2025-06-17 20:37:01 +0200

    ajout &

--- app/queries/find_posts.rb
+++ app/queries/find_posts.rb
@@ -11,13 +11,13 @@ class FindPosts
     scoped = filter_by_mood(scoped, params[:mood])
     scoped = filter_by_friends_only(scoped, current_user, params[:friends_only])
     scoped = filter_by_music_genres(scoped, params[:music_genres])
-    sort(scoped, params[:order_by])
+    scoped
   end
 
   private
 
   def initial_scope
-    Post.order(id: :desc)
+    Post.order(id: :desc).limit(5)
   end
 
   def filter_by_mood(scoped, mood)
@@ -36,14 +36,6 @@ class FindPosts
   def filter_by_music_genres(scoped, music_genres)
     return scoped unless music_genres
      scoped.where("? = ANY(music_genres)", music_genres)
-  end
-
-  def sort(scoped, order_by)
-    order_by_query = Post::ORDER_BY.fetch(order_by&.to_sym, Post::ORDER_BY[:newest])
-    if order_by == :popular
-      scoped = scoped.left_joins(:reactions).group(:id)  #left_jooins pour assiocer chaque post a ses reaction meme si un post n'en a pas, et group pour regrouper les résultats par post
-    end
-    scoped.order(order_by_query)
   end
 
 end
