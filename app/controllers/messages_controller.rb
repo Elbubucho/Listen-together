57ab9ba78697fedf979dbc5f7c79af734f7f872d
Valentin Sala <valmax.sala@gmail.com>
2025-06-18 12:11:04 +0200

    herroku last try avant ticket

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,15 @@ class MessagesController < ApplicationController
           format.html { redirect_to room_path(@message.room) }
         end
       else
-        redirect_to room_path(@message.room), status: :unprocessable_entity
+        respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("new_message_form",
+            partial: "messages/new_message_form", locals: { single_room: @single_room, message: @message })
+          end
+        end
+        format.html { render :show, status: :unprocessable_entity }
       end
   end
-
   private
 
   def msg_params
