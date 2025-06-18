279e5b6bc81bf3bb355bcc011a9163fa04f71b16
Valentin Sala <valmax.sala@gmail.com>
2025-06-18 12:16:51 +0200

    retour en arriere

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,9 @@
 class MessagesController < ApplicationController
+
   def create
-    @single_room = Room.find(params[:room_id])
-    @message = current_user.messages.new(body: msg_params[:body], room_id: params[:room_id])
-      if @message.save
-        respond_to do |format|
-          format.turbo_stream
-          format.html { redirect_to room_path(@message.room) }
-        end
-      else
-        respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("new_message_form",
-            partial: "messages/new_message_form", locals: { single_room: @single_room, message: @message })
-          end
-        end
-        format.html { render :show, status: :unprocessable_entity }
-      end
+    @message = current_user.messages.create(body: msg_params[:body], room_id: params[:room_id])
   end
+
   private
 
   def msg_params
