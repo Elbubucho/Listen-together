74b3a295ab95d26b6c05fd33d7512b7571d9870d
Valentin Sala <valmax.sala@gmail.com>
2025-06-18 12:03:22 +0200

    heroku fix 3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,13 +8,7 @@ class MessagesController < ApplicationController
           format.html { redirect_to room_path(@message.room) }
         end
       else
-        respond_to do |format|
-          format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("new_message_form",
-          partial: "messages/new_message_form", locals: { message: @message })
-          end
-        end
-        format.html { render :show, status: :unprocessable_entity }
+        redirect_to room_path(@message.room), status: :unprocessable_entity
       end
   end
 
