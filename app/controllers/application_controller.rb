0c75c0da76e3c4a6e3f9a98d3ae43873d264e773
Valentin Sala <valmax.sala@gmail.com>
2025-06-18 21:57:57 +0200

    ajout dernier message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_notifications, if: :user_signed_in?
+  before_action :set_current_user
 
   private
 
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  def set_current_user
+    Current.user = current_user
   end
 end
